a03dc69a8c54e5dff5020474273d8a57c9268e62
Madeleine Woodbury <randine25@gmail.com>
2019-03-04 17:26:54 -0500

    Contact form and google map added

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
+// Initialize and add the map
+function initMap() {
+  const loc = { lat: 69.653298, lng: 18.961642 };
+  const map = new google.maps.Map(document.querySelector(".map"), {
+    zoom: 14,
+    center: loc
+  });
+  const marker = new google.maps.Marker({ position: loc, map: map });
+}
+
 // Get the modal & modal-content
 let modal = document.querySelector(".modal");
 let modalContent = document.querySelector(".modal-content");
